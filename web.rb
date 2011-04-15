598110fdafb655f670a2e63a0bc1973ddeef3e01
Dan Nuttall <pixelblend@gmail.com>
2011-04-15 23:53:04 +0100

    Add rescue for non-existant MP3s (usually dead symlinks)

--- web.rb
+++ web.rb
@@ -9,16 +9,21 @@ def get_shows
   shows_dir = File.join(File.dirname(__FILE__), 'public', 'shows')
 
   Dir[File.join(shows_dir, '*.mp3')].collect do |file|
-    s = Show.new
+    begin
+      s = Show.new
 
-    s.url = "shows/#{CGI::escape(File.basename(file))}"
-    s.date = File.mtime(file)
+      s.url = "shows/#{CGI::escape(File.basename(file))}"
+      s.date = File.mtime(file)
 
-    Mp3Info.open(file) do |mp3|
-      s.name = mp3.tag.title || File.basename(file, '.mp3')
-      s.description = [mp3.tag.title,mp3.tag.artist,mp3.tag1.comments].join("\n\r")
+      Mp3Info.open(file) do |mp3|
+        s.name = mp3.tag.title || File.basename(file, '.mp3')
+        s.description = [mp3.tag.title,mp3.tag.artist,mp3.tag1.comments].join("\n\r")
+      end
+
+      s
+    rescue Errno::ENOENT
+      #oops, file probably doesn't exist anymore
+      next
     end
-
-    s
-  end.sort{|x,y| x.date <=> y.date}
+  end.compact.sort{|x,y| x.date <=> y.date}
 end
